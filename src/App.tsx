c1b45de2253e2225df993c3c3c0c9a1ceaae345e
shibajnu <dtlong.2101@gmail.com>
2022-06-16 17:52:30 +0700

    clean branch

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from 'react';
-import Player from './container/Player';
-
-function App() {
-  return <Player />;
-}
-
-export default App;
--- src/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'https://player-backend.herokuapp.com/api/'
