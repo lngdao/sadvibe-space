9ee4b11af08300dc87db45af248576ea1238611f
lngdao <longdao.dev@gmail.com>
2022-06-21 18:06:41 +0700

    fix(progressBar): fix mousemove lag issues

--- src/components/PlayerProgressBar.tsx
+++ src/components/PlayerProgressBar.tsx
@@ -2,10 +2,14 @@ import React, {
   MouseEventHandler,
   MutableRefObject,
   useEffect,
+  useMemo,
+  useRef,
   useState,
 } from 'react';
 import { Navigation2 } from 'react-feather';
+import { useStore } from '../store';
 import { toHHMMSS } from '../utils/funcUtils';
+import _ from 'lodash';
 
 interface Props {
   player: HTMLAudioElement;
@@ -20,47 +24,69 @@ function PlayerProgressBar({
   player,
   playerTime,
 }: Props) {
+  const { theme } = useStore((state) => state.setting);
   const [progress, setProgress] = useState({
     isDrag: false,
     percent: 0,
     spanedTime: '00:00',
   });
 
+  const percentRef = useRef<number | null>(null);
+  const dragTriggerRate = useMemo(() => {
+    return (1 / player.duration) * 100
+  }, [player.duration])
+
   const onProgressBarClickDown: MouseEventHandler<HTMLDivElement> = (e) => {
     isReadyToDragRef.current = true;
   };
 
   const registerDocumentListener = () => {
-    document.addEventListener('mousemove', (e) => {
-      const progress = document.getElementById('progress')!;
+    document.addEventListener(
+      'mousemove',
+      _.debounce((e) => {
+        const progress = document.getElementById('progress')!;
 
-      const clientX = e.clientX;
-      const progressWidth = progress.getBoundingClientRect().width;
-      const progressLeft = progress.getBoundingClientRect().left;
+        const clientX = e.clientX;
+        const progressWidth = progress.getBoundingClientRect().width;
+        const progressLeft = progress.getBoundingClientRect().left;
 
-      const min = progressLeft;
-      const max = progressWidth + progressLeft;
+        const min = progressLeft;
+        const max = progressWidth + progressLeft;
 
-      if (isReadyToDragRef.current && clientX >= min && clientX <= max) {
-        const percent = ((clientX - min) / progressWidth) * 100;
+        if (isReadyToDragRef.current && clientX >= min && clientX <= max) {
+          const percent = ((clientX - min) / progressWidth) * 100;
+          if (
+            !percentRef.current ||
+            percent - percentRef.current >= dragTriggerRate ||
+            percentRef.current - percent >= dragTriggerRate ||
+            percent == 0
+          ) {
+            playerSpanedTimeRef.current = (percent / 100) * player.duration;
+            const progressSpanedTime = toHHMMSS(
+              parseInt(
+                ((percent / 100) * player.duration).toString(),
+                10
+              ).toString()
+            );
 
-        playerSpanedTimeRef.current = (percent / 100) * player.duration;
-        const progressSpanedTime = toHHMMSS(
-          parseInt(
-            ((percent / 100) * player.duration).toString(),
-            10
-          ).toString()
-        );
+            console.log(percent);
 
-        setProgress({ isDrag: true, percent, spanedTime: progressSpanedTime });
-      }
-    });
+            setProgress({
+              isDrag: true,
+              percent,
+              spanedTime: progressSpanedTime,
+            });
+            percentRef.current = percent;
+          }
+        }
+      })
+    );
 
     document.addEventListener('mouseup', (e) => {
       if (isReadyToDragRef.current) {
         player.currentTime = playerSpanedTimeRef.current;
         isReadyToDragRef.current = false;
-        setProgress((prev) => ({ ...prev, isDrag: false }));
+        // setProgress((prev) => ({ ...prev, isDrag: false }));
       }
     });
   };
@@ -94,10 +120,14 @@ function PlayerProgressBar({
         <div
           className="done"
           style={{
+            background: theme.value.content,
             right: `${100 - progress.percent}%`,
           }}
         ></div>
-        <div className="remaining"></div>
+        <div
+          className="remaining"
+          style={{ background: theme.value.content }}
+        ></div>
       </div>
       <div
         className="handle-ctr"
@@ -106,7 +136,13 @@ function PlayerProgressBar({
           left: `${progress.percent}%`,
         }}
       >
-        <p className="time-span">
+        <p
+          style={{
+            color: theme.value.primary,
+            background: theme.value.content,
+          }}
+          className="time-span"
+        >
           {!isReadyToDragRef.current
             ? playerTime.currentTime
             : progress.spanedTime}
@@ -114,8 +150,8 @@ function PlayerProgressBar({
         <Navigation2
           className="handle"
           size={15}
-          fill={'#333'}
-          color={'#333'}
+          fill={theme.value.content}
+          color={theme.value.content}
         />
       </div>
     </div>
