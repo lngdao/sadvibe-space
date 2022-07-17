06dd4286d896c8e0019f149e71b7d8ce02905f1a
lngdao <longdao.dev@gmail.com>
2022-07-17 19:56:35 +0700

    fix: update empty with lottie animation

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Frown } from 'react-feather';
+import React, { memo, useMemo } from 'react';
+import { Meh } from 'react-feather';
 import { useStore } from '../store';
+import Lottie from 'lottie-react';
+import catLightMode from '../asset/lottieJson/catLightMode.json';
+import catDarkMode from '../asset/lottieJson/catDarkMode.json';
 
 interface Props {
   emptyTxt: string;
+  lottie?: boolean;
 }
 
-function Empty({ emptyTxt }: Props) {
+function Empty({ emptyTxt, lottie }: Props) {
   const { theme } = useStore((state) => state.setting);
+
+  const jsonThemeData: any = useMemo(
+    () => (theme.type == 'light' ? catLightMode : catDarkMode),
+    [theme.type]
+  );
 
   return (
     <div className="emptyList">
-      <Frown color={theme.value.content} size={40} />
+      {lottie ? (
+        <Lottie animationData={jsonThemeData} loop />
+      ) : (
+        <Meh color={theme.value.content} size={40} />
+      )}
       <h4 style={{ color: theme.value.content }}>{emptyTxt}</h4>
     </div>
   );
 }
 
-export default Empty;
+export default memo(Empty);
