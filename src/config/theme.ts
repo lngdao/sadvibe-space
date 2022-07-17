238992faeb8b983055b78fde37800829e3d873ef
lngdao <longdao.dev@gmail.com>
2022-07-17 19:55:30 +0700

    chore(theme): change color theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -2,14 +2,18 @@ const dark = {
   primary: '#2B2B2B',
   title: '#EEEEEE',
   content: '#EEEEEE',
-  grey: '#7E7474'
+  grey: '#AAAAAA',
+  highlight: '#E1D89F',
+  switch: '#839B97'
 };
 
 const light = {
   primary: '#f9f9f9',
   title: '#333',
   content: '#354259',
-  grey: '#6C7A89'
+  grey: '#6C7A89',
+  highlight: '#B09B71',
+  switch: '#C9D8B6'
 };
 
 export { dark, light };
